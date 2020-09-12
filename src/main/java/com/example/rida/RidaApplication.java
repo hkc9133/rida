af422a2b50320b00c45bbc9cbe98f5803d9da823
eney <hkc9133@naver.com>
2020-09-13 06:40:02 +0900

    Initial commit

---
+++ src/main/java/com/example/rida/RidaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RidaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rida/RidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
